64b5db15b5b3a008e1fc065d473cf666ce294050
Lawrence Walters <lwalters@lyonscg.com>
2018-02-27 20:17:52 -0600

    include parsing of timesheet file

--- tenrox.js
+++ tenrox.js
@@ -9,7 +9,6 @@ TENROX_ORG - organization / company code for your tenrox instance. generally cas
 
 execute this script like:
 $ TENROX_USER=asdf TENROX_PASS="bar" TENROX_HOST=acme.tenrox.net TENROX_ORG=Acme node tenrox.js
-
 */
 
 var q = require('q');
@@ -17,110 +16,40 @@ var request = require('request');
 var util = require('util');
 var fs = require('fs');
 var prettyjson = require('prettyjson');
+var winston = require('winston');
+var colors = require('colors');
+
+var colorMap = new Map();
+winston.level = process.env.LOG_LEVEL;
+
 var cookie = null;
 var opts = null;
 
-/*
-auth - get a token
-get unique user id (requires auth)
-get current timesheet (gets tasks, timesheet id, unique user id)
-write an entry (needs task id, timesheet id)
+getSession(process.env.TENROX_HOST,
+    process.env.TENROX_ORG,
+    process.env.TENROX_USER,
+    process.env.TENROX_PASS)
+    .then(processTimeSheet)
+    .catch(function (error) {
+        console.log("Error: " + error);
+    });
 
-getTimeSheetInfo(session, unique user id, date) returns timesheet id, [{task name, id}]
-
-assignTaskIds(session, entries) returns entries [{task id, date, hours, notes}]
-
-writeEntry(session, timesheet id, task, date, hours, notes) returns promise
-
-getSession()
-    .then(getUniqueUserId)
-    .then(getTimeSheetInfo)
-    .then(getEntries)
-    .then(assignTaskIds)
-    .then(writeEntries);
-
-*/
-
-function auth(callback) {
-    if (opts != null) {
-        console.log('Using cached token');
-        callback(null, opts);
-        // }
-        // else if (fs.existsSync('./access_token.json')) {
-        //     console.log("loading token from disk");
-        //     access_token = fs.readFileSync('./access_token.json', 'utf8');
-        //     getUserId(access_token, callback);
-    }
-    else {
-        console.log('getting new token');
-        request.post({
-            url: 'https://' + process.env.TENROX_HOST + '/TEnterprise/api/token',
-            headers: { OrgName: process.env.TENROX_ORG },
-            body: 'grant_type=password&username=' + process.env.TENROX_USER + '&password=' + encodeURIComponent(process.env.TENROX_PASS)
-        },
-            function (error, response, body) {
-                if (error) {
-                    callback(error);
-                    return null;
-                }
-                cookie = response.headers['set-cookie'];
-                access_token = JSON.parse(body).access_token;
-                fs.writeFileSync('./access_token.json', access_token, 'utf-8');
-                getUserId(access_token, callback);
-            });
-    }
-}
-
-function getOpts(token) {
-    var auth = "Bearer " + token;
-    return {
-        headers: {
-            "Cookie": cookie,
-            "authorization": auth,
-            "Content-Type": "application/json",
-            OrgName: process.env.TENROX_ORG
-        }
-    };
-}
-
-function getUserId(access_token, callback) {
-    opts = getOpts(access_token);
-    request.get({
-        headers: opts.headers,
-        url: "https://" + process.env.TENROX_HOST + "/TEnterprise/api/v2/Users/?$filter=LoginName eq '" + process.env.TENROX_USER + "'"
-    },
-        function (error, response, body) {
-            if (error) {
-                callback("Error looking up user id: " + error);
-                return null;
-            } else if (body == 'Invalid token.') {
-                callback("Error looking up user id: invalid token");
-                return null;
-            }
-
-            opts.user_UniqueId = JSON.parse(body)[0].UniqueId;
-            callback(null, opts);
+function processTimeSheet(session) {
+    defer = q.defer();
+    getUniqueUserId(session)
+        .then(function (uniqueUserId) {
+            return getTimeSheetInfo(session, uniqueUserId, new Date());
+        })
+        .then(processFile)
+        // .then(assignTaskIds)
+        // .then(writeEntries);
+        .then(console.log)
+        .catch(function (error) {
+            console.log("Error: " + error);
         });
-}
+    // TODO: check totals and update if different?
 
-function makeEntry() {
-    auth(function (error, opts) {
-        if (error) {
-            console.log(error);
-            return null;
-        }
-        console.log("getting timesheet for user_id " + opts.user_UniqueId);
-        var now = new Date();
-
-        request.get({
-            headers: opts.headers,
-            url: "https://" + process.env.TENROX_HOST + "/TEnterprise/api/Timesheets/?UserId=" + opts.user_UniqueId +
-            "&anyDate=" + ("0" + (now.getMonth() + 1)).slice(-2) + '-'
-            + ("0" + now.getDate()).slice(-2) + '-'
-            + now.getFullYear()
-        },
-            logTimeWithNotes);
-    })
+    return defer.promise;
 }
 
 function logTimeWithNotes(error, response, body) {
@@ -250,32 +179,43 @@ function getTimeSheetInfo(session, uniqueUserId, date) { // returns timesheet id
     return defer.promise;
 }
 
-function processTimeSheet(session) {
+function processFile() {
+    var filename = 'c:\\users\\lwalters\\docs\\proj\\timesheet_data.txt'
     defer = q.defer();
-    getUniqueUserId(session)
-        .then(function (uniqueUserId) {
-            return getTimeSheetInfo(session, uniqueUserId, new Date());
-        })
-        // .then(getEntries)
-        // .then(assignTaskIds)
-        // .then(writeEntries);
-        .then(console.log)
-        .catch(function (error) {
-            console.log("Error: " + error);
-        });
-    // TODO: map task IDs to what goes in the notes (TaskUid)
-    //   - get a list of tasks ?
-    // TODO: 1x update of all things
-    // TODO: check totals and update if different?
+    var summary = {};
+    var current = '';
+    var daytotal = 0;
+    var lineReader = require('readline').createInterface({
+        input: require('fs').createReadStream(filename)
+    });
 
+    lineReader.on('line', function (input) { parse(input, summary) });
+    lineReader.on('close', function () {
+        defer.resolve(summary);
+    });
     return defer.promise;
 }
 
-getSession(process.env.TENROX_HOST,
-    process.env.TENROX_ORG,
-    process.env.TENROX_USER,
-    process.env.TENROX_PASS)
-    .then(processTimeSheet)
-    .catch(function (error) {
-        console.log("Error: " + error);
-    });
+function parse(line, summary) {
+    winston.log('info', line);
+    if (line[0] == '#')
+        return;
+    if (line.match(/^\d+\/\d+$/)) {
+        current = line;
+        summary[current] = {
+            'daytotal': 0
+        };
+    } else if (line.match(/^([^,]+),(.+)\b(\d+)$/)) {
+        match = line.match(/^([^,]+),(.+)\b(\d+)$/);
+        winston.log('info', ' id:', match[1], '\n comments:', match[2], '\n minutes:', match[3]);
+        if (!(match[1] in summary[current])) {
+            summary[current][match[1]] = {
+                'comments': '',
+                'minutes': 0
+            };
+        }
+        summary[current][match[1]].comments += match[2];
+        summary[current][match[1]].minutes += Number(match[3]);
+        summary[current].daytotal += Number(match[3]);
+    }
+}
